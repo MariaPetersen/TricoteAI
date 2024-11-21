617f6773836259653d3c86da3946083aac99e4c6
Maria Petersen <maria.petersen@ealico.com>
2024-11-21 16:10:53 +0100

    IMPROVE add name conditions in file

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ args = parser.parse_args()
 
 OllamaFunctions = Ollama(args.model)
 
-while True:
-    user_input = input(YELLOW + "Ask something about knitting (or type 'quit' to exit and add '--NORAG' to query without document information): " + RESET_COLOR)
-    if user_input.lower() == 'quit':
-        break
-    print(CYAN + "Let me think about all this..." + RESET_COLOR)
-    
-    if "--NORAG" in user_input:
-        response = OllamaFunctions.ollama_no_rag_chat(user_input)
-    else:
-        response = OllamaFunctions.ollama_chat(user_input)
-    print(NEON_GREEN + "Response: \n\n" + response + RESET_COLOR)
+if __name__ == "__main__":
+    while True:
+        user_input = input(YELLOW + "Ask something about knitting (or type 'quit' to exit and add '--NORAG' to query without document information): " + RESET_COLOR)
+        if user_input.lower() == 'quit':
+            break
+        print(CYAN + "Let me think about all this..." + RESET_COLOR)
+        
+        if "--NORAG" in user_input:
+            response = OllamaFunctions.ollama_no_rag_chat(user_input)
+        else:
+            response = OllamaFunctions.ollama_chat(user_input)
+        print(NEON_GREEN + "Response: \n\n" + response + RESET_COLOR)

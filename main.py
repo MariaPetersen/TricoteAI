d6d8dc731630126d08527ed223c905a82047c0ac
Maria Petersen <maria.petersen@ealico.com>
2024-11-21 16:23:50 +0100

    IMPROVE add private functions to ollama functions class

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 
 from ollama_functions import Ollama
 import argparse
-from colors import PINK, CYAN, YELLOW, NEON_GREEN, RESET_COLOR
-   
-parser = argparse.ArgumentParser(description="Ollama Chat")
-parser.add_argument("--model", default="llama3.2", help="Ollama model to use (default: llama3.2)")
-args = parser.parse_args()
-
-OllamaFunctions = Ollama(args.model)
+from colors import CYAN, YELLOW, NEON_GREEN, RESET_COLOR
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Ollama Chat")
+    parser.add_argument("--model", default="llama3.2", help="Ollama model to use (default: llama3.2)")
+    args = parser.parse_args()
+
+    ollama_class = Ollama(args.model)
     while True:
         user_input = input(YELLOW + "Ask something about knitting (or type 'quit' to exit and add '--NORAG' to query without document information): " + RESET_COLOR)
         if user_input.lower() == 'quit':
@@ -17,7 +16,7 @@ if __name__ == "__main__":
         print(CYAN + "Let me think about all this..." + RESET_COLOR)
         
         if "--NORAG" in user_input:
-            response = OllamaFunctions.ollama_no_rag_chat(user_input)
+            response = ollama_class.ollama_no_rag_chat(user_input)
         else:
-            response = OllamaFunctions.ollama_chat(user_input)
+            response = ollama_class.ollama_chat(user_input)
         print(NEON_GREEN + "Response: \n\n" + response + RESET_COLOR)
--- ollama_functions.py
+++ ollama_functions.py
@@ -6,57 +6,60 @@ from openai import OpenAI
 from colors import PINK, CYAN, YELLOW, NEON_GREEN, RESET_COLOR
 
 class Ollama():
-    def __init__(self, ollama_model):
-        self.model_url = 'http://localhost:11434/v1'
-        self.embeddings_file = "vault_embeddings.pt"
-        self.vault_content = self.get_vault_content()
-        self.client = OpenAI(
-                base_url=self.model_url,
-                api_key='llama3'
-            )
-        self.conversation_history = []
-        self.system_message = "You are a helpful assistant that is an expert in knitting by extracting the most useful tips from the given document."
+    def __init__(self, ollama_model, embeddings_file="vault_embeddings.pt", vault_file="vault.txt", api_key="llama3"):
+        self.model_url = "http://localhost:11434/v1"
+        self.embeddings_file = embeddings_file
+        self.vault_file = vault_file
         self.ollama_model = ollama_model
+        self.client = OpenAI(base_url=self.model_url, api_key=api_key)
+        self.system_message = (
+            "You are a helpful assistant that is an expert in knitting by extracting the most useful tips from the given document."
+        )
+        self.conversation_history = []
+        self.vault_content = self._load_vault_content()
         
-    def save_embeddings(self, embeddings, filepath):
+    def _save_embeddings(self, embeddings, filepath):
         torch.save(embeddings, filepath)
         
-    def load_embeddings(self, filepath):
+    def _load_embeddings(self, filepath):
         if os.path.exists(filepath):
             return torch.load(filepath, weights_only=False)
         return None
     
-    def get_vault_content(self):
-        vault_content = []
-        if os.path.exists("vault.txt"):
-            with open("vault.txt", "r", encoding='utf-8') as vault_file:
-                vault_content = vault_file.readlines()
-        return vault_content
+    def _load_vault_content(self):
+        if os.path.exists(self.vault_file):
+            with open(self.vault_file, "r", encoding="utf-8") as file:
+                return file.readlines()
+        return []
     
-    def get_relevant_context(self, rewritten_input, top_k=3):
-        vault_embeddings = self.get_vault_embeddings_tenor()
+    def _generate_embeddings(self, content):
+        response = ollama.embeddings(model="mxbai-embed-large", prompt=content)
+        return response["embedding"]
+    
+    def _get_vault_embeddings(self):
+        embeddings = self._load_embeddings()
+        if embeddings is None:
+            embeddings = [
+                self._generate_embeddings(content) for content in self.vault_content
+            ]
+            embeddings_tensor = torch.tensor(embeddings)
+            self._save_embeddings(embeddings_tensor)
+            return embeddings_tensor
+        return embeddings
+    
+    def _get_relevant_context(self, rewritten_input, top_k=3):
+        vault_embeddings = self._get_vault_embeddings()
         if vault_embeddings.nelement() == 0:
             return []
-        input_embedding = ollama.embeddings(model='mxbai-embed-large', prompt=rewritten_input)["embedding"]
-        cos_scores = torch.cosine_similarity(torch.tensor(input_embedding).unsqueeze(0), vault_embeddings)
-        top_k = min(top_k, len(cos_scores))
-        top_indices = torch.topk(cos_scores, k=top_k)[1].tolist()
-        relevant_context = [self.vault_content[idx].strip() for idx in top_indices]
-        return relevant_context
+        input_embedding = torch.tensor(
+            self._generate_embeddings(rewritten_input)
+        ).unsqueeze(0)
+        cos_scores = torch.cosine_similarity(input_embedding, vault_embeddings)
+        top_indices = torch.topk(cos_scores, k=min(top_k, len(cos_scores))).indices.tolist()
+        return [self.vault_content[idx].strip() for idx in top_indices]
     
-    def get_vault_embeddings_tenor(self):
-        vault_embeddings_tensor = self.load_embeddings(self.embeddings_file)
-
-        if vault_embeddings_tensor is None:
-            vault_embeddings = []
-            for content in self.vault_content:
-                response = ollama.embeddings(model='mxbai-embed-large', prompt=content)
-                vault_embeddings.append(response["embedding"])
-            vault_embeddings_tensor = torch.tensor(vault_embeddings) 
-            self.save_embeddings(vault_embeddings_tensor, self.embeddings_file)
-        return vault_embeddings_tensor
     
-    def rewrite_query(self, user_input_json):
+    def _rewrite_query(self, user_input_json):
         user_input = json.loads(user_input_json)["Query"]
         user_input.replace("--NORAG", "")
         context = "\n".join([f"{msg['role']}: {msg['content']}" for msg in self.conversation_history[-2:]])
@@ -84,57 +87,42 @@ class Ollama():
             n=1,
             temperature=0.1,
         )
-        rewritten_query = response.choices[0].message.content.strip()
-        return json.dumps({"Rewritten Query": rewritten_query})
+        return json.dumps({"Rewritten Query": response.choices[0].message.content.strip()})
     
-    
-    def get_rewritten_query(self, user_input):
+    def _get_rewritten_query(self, user_input):
         if len(self.conversation_history) > 1:
-            query_json = {
-                "Query": user_input,
-                "Rewritten Query": ""
-            }
-            rewritten_query_json = self.rewrite_query(json.dumps(query_json))
+            query_json = json.dumps({"Query": user_input, "Rewritten Query": ""})
+            rewritten_query_json = self._rewrite_query(query_json)
             rewritten_query_data = json.loads(rewritten_query_json)
-            rewritten_query = rewritten_query_data["Rewritten Query"]
-        else:
-            rewritten_query = user_input
-        return rewritten_query
-    
-    def ask_ollama(self):
+            return rewritten_query_data["Rewritten Query"]
+        return user_input
+        
+    def _ask_ollama(self):
         messages = [
             {"role": "system", "content": self.system_message},
-            *self.conversation_history
+            *self.conversation_history,
         ]
-        
         response = self.client.chat.completions.create(
-            model=self.ollama_model,
-            messages=messages,
-            max_tokens=2000,
+            model=self.ollama_model, messages=messages, max_tokens=2000
         )
-        
-        self.conversation_history.append({"role": "assistant", "content": response.choices[0].message.content})
-        
-        return response.choices[0].message.content
-        
+        assistant_message = response.choices[0].message.content.strip()
+        self.conversation_history.append({"role": "assistant", "content": assistant_message})
+        return assistant_message
+    
     def ollama_chat(self, user_input):
         self.conversation_history.append({"role": "user", "content": user_input})
-        rewritten_query = self.get_rewritten_query(user_input)
-        relevant_context = self.get_relevant_context(rewritten_query)
+        rewritten_query = self._get_rewritten_query(user_input)
+        relevant_context = self._get_relevant_context(rewritten_query)
+
         if relevant_context:
             context_str = "\n".join(relevant_context)
-        
-        user_input_with_context = user_input
-        if relevant_context:
-            user_input_with_context = user_input + "\n\nRelevant Context:\n" + context_str
-        
-        self.conversation_history[-1]["content"] = user_input_with_context
-        
-        return self.ask_ollama()
+            user_input += f"\n\nRelevant Context:\n{context_str}"
+
+        self.conversation_history[-1]["content"] = user_input
+        return self._ask_ollama()
     
     def ollama_no_rag_chat(self, user_input):
         self.conversation_history.append({"role": "user", "content": user_input})
-        rewritten_query = self.get_rewritten_query(user_input)
+        rewritten_query = self._get_rewritten_query(user_input)
         self.conversation_history[-1]["content"] = rewritten_query
-        
-        return self.ask_ollama()
+        return self._ask_ollama()
